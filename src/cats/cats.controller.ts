cd90c76f39a01c6dbe2a55a33380670ffd01772c
Yuki Komada <y.komada.jp@gmail.com>
2022-05-15 02:47:55 +0000

    パイプのお勉強

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, UseFilters, HttpStatus, HttpException } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, UseFilters, HttpStatus, HttpException } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 import { HttpExceptionFilter } from 'src/common/filter/http-exception.filter';
+import { SamplePipe } from 'src/common/pipe/sample.pipe';
 
 @Controller('cats')
 export class CatsController {
@@ -11,11 +12,16 @@ export class CatsController {
     @Post()
     @UseFilters(new HttpExceptionFilter())
     async create(@Body() createCatDto: CreateCatDto) {
-        throw new HttpException("エラーメッセージ", HttpStatus.FORBIDDEN);
+        this.catsService.create(createCatDto);
     }
 
     @Get()
     async findAll(): Promise<Cat[]> {
         throw new HttpException("エラーメッセージ", HttpStatus.FORBIDDEN);
+    }
+
+    @Get(':id')
+    async findOne(@Param('id', SamplePipe) id: number) {
+        return this.catsService.findOne(id);
     }
 }
---
+++ src/common/pipe/sample.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { SamplePipe } from './sample.pipe';
+
+describe('SamplePipe', () => {
+  it('should be defined', () => {
+    expect(new SamplePipe()).toBeDefined();
+  });
+});
